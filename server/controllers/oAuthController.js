cdd790ff739a930b00a495ea22beaff289dcdc37
chcho2 <chcho87@gmail.com>
2020-10-07 14:14:21 -0400

    updated OAuth

--- server/controllers/oAuthController.js
+++ server/controllers/oAuthController.js
@@ -1,25 +1,23 @@
-const OAuth2Data = require('../oAuth.env');
+
 const {
     OAuth2Client
 } = require('google-auth-library');
 
-const CLIENT_ID = OAuth2Data.client.id;
-const CLIENT_SECRET = OAuth2Data.client.secret;
-const REDIRECT_URL = OAuth2Data.client.redirect;
 
 
 const oAuthController = {};
 
 
 oAuthController.verifyToken = (req, res, next) => {
-  const client = new OAuth2Client(CLIENT_ID);
+ 
+  const client = new OAuth2Client(process.env.CLIENT_ID);
   const { token } = req.body;
-
+  
   async function verify() {
       
           const ticket = await client.verifyIdToken({
             idToken: token,
-            audience: CLIENT_ID,
+            audience: process.env.CLIENT_ID,
           });
           const payload = ticket.getPayload();
           const userId = payload['sub'];
--- server/server.js
+++ server/server.js
@@ -1,25 +1,29 @@
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const app = express();
-const PORT = 5000;
+const PORT = 3000;
 
 // routers
 const businessesRouter = require('./routes/businesses.js');
 const locationRouter = require('./routes/location.js');
 const newsRouter = require('./routes/news.js');
 const weatherRouter = require('./routes/weather.js');
+const signinRouter = require('./routes/signin.js');
 
 // application-level middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
 
 // route handlers
 app.use('/businesses', businessesRouter);
 app.use('/location', locationRouter);
 app.use('/news', newsRouter);
 app.use('/weather', weatherRouter);
+app.use('/signin', signinRouter);
 
 if (process.env.NODE_ENV === 'production') {
   app.use('/build', express.static(path.resolve(__dirname, '..build')));
